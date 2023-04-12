b0231d50ce6d04eba952ba8f3b82d81f6548fc3c
dapt <diegodapt18@gmail.com>
2023-04-12 17:28:00 +0200

    adding the models to the project

---
+++ models/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
+
+class Base(DeclarativeBase):
+    pass
+
+
+
+class Task(Base):
+    __tablename__ = "task"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(30))
+    description: Mapped[str] = mapped_column(String(200))
+
+    def __repr__(self) -> str:
+        return f'Task(id={self.id}, title={self.title}, description={self.description})'
